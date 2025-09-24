c65294d76f0aabd962dfb25eff9f1df83b63061f
jianglang <lang.jiang@ximalaya.com>
2025-09-24 23:42:42 +0800

    feat: 在 swellDraw 包中添加测试语言支持，优化国际化功能，新增语言翻译方法及相关钩子

--- packages/swellDraw/i18n.ts
+++ packages/swellDraw/i18n.ts
@@ -1,6 +1,7 @@
-import { NestedTypeOf } from "@swell-draw/common";
+import { isDevEnv, NestedTypeOf } from "@swell-draw/common";
 import fallbackLangData from "./locales/zh-cN.json";
 import percentages from "./locales/precentages.json";
+import { atom, useAtomValue } from "./editor-jotai";
 
 const COMPLETION_THRESHOLD = 85;
 
@@ -31,6 +32,18 @@ export const languages: Language[] = [
   )
   .sort((left, right) => (left.label > right.label ? 1 : -1));
 
+const TEST_LANG_CODE = "__test__";
+if (isDevEnv()) {
+  languages.unshift(
+    { code: TEST_LANG_CODE, label: "test language" },
+    {
+      code: `${TEST_LANG_CODE}.rtl`,
+      label: "\u{202a}test language (rtl)\u{202c}",
+      rtl: true,
+    },
+  );
+}
+
 let currentLang: Language = defaultLang;
 let currentLangData = fallbackLangData;
 
@@ -47,4 +60,66 @@ export const setLanguage = async (lang: Language) => {
   }
 };
 
-console.log(currentLangData);
+export const getLanguage = () => currentLang;
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const findPartsForData = (data: Record<string, any>, parts: string[]) => {
+  for (let index = 0; index < parts.length; ++index) {
+    const part = parts[index];
+    if (data[part] === undefined) {
+      return undefined;
+    }
+    data = data[part];
+  }
+  if (typeof data !== "string") {
+    return undefined;
+  }
+  return data;
+};
+
+export const t = (
+  path: TranslationKeys,
+  replacement?: { [key: string]: string | number } | null,
+  fallback?: string,
+) => {
+  if (currentLang.code.startsWith(TEST_LANG_CODE)) {
+    const name = replacement
+      ? `${path}(${JSON.stringify(replacement).slice(1, -1)})`
+      : path;
+    return `\u{202a}[[${name}]]\u{202c}`;
+  }
+
+  const parts = path.split(".");
+  let translation =
+    findPartsForData(currentLangData, parts) ||
+    findPartsForData(fallbackLangData, parts) ||
+    fallback;
+  if (translation === undefined) {
+    const errorMessage = `Can't find translation for ${path}`;
+    // 在生产环境中，不要因为缺少翻译键而使应用崩溃
+    if (import.meta.env.PROD) {
+      console.warn(errorMessage);
+      return "";
+    }
+    throw new Error(errorMessage);
+  }
+
+  if (replacement) {
+    for (const key in replacement) {
+      translation = translation.replace(`{{${key}}}`, String(replacement[key]));
+    }
+  }
+  return translation;
+};
+
+/** @private 仅用于重新渲染使用 `useI18n` 钩子的组件的 atom */
+const editorLangCodeAtom = atom(defaultLang.code);
+
+// 应该在以下情况的组件中使用：
+// - 组件作为 <Excalidraw> 的子组件渲染
+// - 组件由 <Excalidraw> 内部渲染，但该组件
+//   被记忆化且未在 `langCode`、`AppState` 或 `UIAppState` 更新时更新
+export const useI18n = () => {
+  const langCode = useAtomValue(editorLangCodeAtom);
+  return { t, langCode };
+};
